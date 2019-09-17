df26f86f0aacab57ac54c058b2051aca4cae5486
ha382 <ha382@njit.edu>
2019-09-17 12:18:25 -0400

    test

--- initDB.php
+++ initDB.php
@@ -17,5 +17,14 @@ catch(Expception $e){
       exit9"It didn`t work");
 }
 ?>
-
+$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
+}
 
