b1b4a0e5ae0d617bc8afea11f61a44bfe9216a5a
ha382 <ha382@njit.edu>
2019-10-03 12:43:42 -0400

    adding myForm.php

---
+++ handleRequestData.php
@@ -0,0 +1,37 @@
+<?php
+
+
+if(isset($_GET['name'])){
+	echo "<br>Hello, " . $_GET['name'] . "<br>";
+}
+echo "<pre>" . var_export($_GET, true) . "</pre>";
+
+if(isset($_GET['number'])){
+	$number1 = $_GET['number'];
+	echo "<br>" . $number . " should be a number...";
+	echo "<br>but it might not be<br>";
+
+if(isset($_GET['number'])){
+        $number1 = $_GET['number'];
+
+if(isset($_GET['number'])){
+        $number2 = $_GET['number'];
+
+if(isset ($number0) &&isset(number1) && isset(number2)){
+            if(is_numeric($numbr0) && is_numeric(&number1) && is_numeric{&number2}0
+  $number0 = (int)$number0;
+  $number1 = (int)$number1;
+  $number2 = (int)$number2;
+echo "sum: " . ($number0 + $number1 + $number2);
+}
+if(isset($_GET['parameter'])){
+echo "<div>" . $_GET['parameter'] . "</div>;
+}
+}
+catch{Expection $e){
+echo $e->getMessage();
+exit("Something went wrong"):
+}
+?>
+
+
--- initDB.php
+++ initDB.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 require(`config.php`);
 echo $host;
 ?>
-$connection_string="mysql:host=$host;dbname=$database;username=$username;password=$password;charset=utf8mb4";
+$connection_string="mysql:host=$host;dbname=$database;username=$password;charset=utf8mb4";
 try{
       $db = new PDO($connection_string);
       echo "should have connected";
@@ -28,3 +28,32 @@ $query = "create table if not exists `TestUsers`(
 	echo "<br>" . $r . "<br>";
 }
 
+<?php
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set(`display_startup_errors`,1);
+//pull in config.php so we can access the variable from it
+require(`confiq.php`);
+echo "loaded Host:" . $host;
+
+$conn_string = "mysqul:$host;dbname=$database;charset=utf8mb4";
+
+try{
+
+ $db = new PDO($conn_string, $username, $password);
+ echo "Connected
+$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("Something went wrong");
+}
+?>
