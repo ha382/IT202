edbddeefa6cef8fef840ddfe56eaf3988da1f61b
ha382 <ha382@njit.edu>
2019-11-14 11:54:34 -0500

    longin.php

---
+++ Project/register.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<h>Registration page</h><br>
+
+<input name="name" type="text" placeholder="Enter your name"/>
+<input type="password" name="password" placeholder="Enter password"/>
+</body>
+</html>
+
+
